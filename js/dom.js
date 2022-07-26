96f03171ba4bc7fec2709478d2a8f8a4ce1c541c
Seprianto Sianipar <raysianipar15@gmail.com>
2022-07-27 00:39:19 +0700

    update responsive header

--- js/dom.js
+++ js/dom.js
@@ -4,34 +4,6 @@ const SAVED_EVENT = 'saved-book';
 const STORAGE_KEY = 'BOOKSHELF_APPS';
 
 
-const saveData = () => {
-    if (isStorageExist()) {
-        const parsed =JSON.stringify(books);
-        localStorage.setItem(STORAGE_KEY, parsed);
-        document.dispatchEvent(new Event(SAVED_EVENT));
-    }
-}
-
-const isStorageExist = () => {
-    if (typeof(Storage) === undefined) {
-        alert('Browser kamu tidak mendukung local storage');
-        return false;
-    }
-    return true;
-}
-
-const loadDataFromStorage = () => {
-    const serializedData = localStorage.getItem(STORAGE_KEY);
-    let data = JSON.parse(serializedData);
-
-    if (data !== null) {
-        for (const book of data) {
-            books.push(book);
-        }
-    }
-    document.dispatchEvent(new Event(RENDER_EVENT));
-}
-
 // Menambah buku
 const addBook = () => {
     const textTitle = document.getElementById('inputTitle').value;
@@ -196,4 +168,32 @@ const searchTitleBook = () => {
                 titleBook.setAttribute('style', 'display : none');
             }
         }
+}
+
+const saveData = () => {
+    if (isStorageExist()) {
+        const parsed =JSON.stringify(books);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+const isStorageExist = () => {
+    if (typeof(Storage) === undefined) {
+        alert('Browser kamu tidak mendukung local storage');
+        return false;
+    }
+    return true;
+}
+
+const loadDataFromStorage = () => {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+
+    if (data !== null) {
+        for (const book of data) {
+            books.push(book);
+        }
+    }
+    document.dispatchEvent(new Event(RENDER_EVENT));
 }
